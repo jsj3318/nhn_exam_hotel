54f0f29c92df776451c6dedcdabb7c4eb21f8c16
조승주 <tmdwn3318@gmail.com>
2025-01-06 09:28:07 +0900

    feat: room 엔티티 작성

--- src/main/java/com/nhnacademy/exam/hotel/domain/Room.java
+++ src/main/java/com/nhnacademy/exam/hotel/domain/Room.java
@@ -1,5 +1,43 @@
 package com.nhnacademy.exam.hotel.domain;
 
-public class Room {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roomId;
+
+    @NotNull
+    @ManyToOne
+    private Hotel hotel;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private int capacity;
+
+    @NotNull
+    private int floor;
+
+    @NotNull
+    private boolean bathtubFlag = true;
+
+    @NotNull
+    private int viewType;
+
+    @NotNull
+    private LocalDateTime createdAt;
 }

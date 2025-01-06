a65954a4a2b40c7c47167b5f43bf87bc7e9f6207
조승주 <tmdwn3318@gmail.com>
2025-01-06 14:35:50 +0900

    feat: 예약 엔티티

--- src/main/java/com/nhnacademy/exam/hotel/domain/Reservation.java
+++ src/main/java/com/nhnacademy/exam/hotel/domain/Reservation.java
@@ -1,5 +1,38 @@
 package com.nhnacademy.exam.hotel.domain;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
 public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reservationId;
+
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    private LocalDateTime checkIn;
+
+    @NotNull
+    private LocalDateTime checkOut;
+
+    @NotNull
+    private int peopleCount;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
 }

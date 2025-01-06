32bf7d602773c920675e7b89136a1267ba780fc6
조승주 <tmdwn3318@gmail.com>
2025-01-06 10:56:07 +0900

    feat: dbcp2 DataSource 설정

--- src/main/java/com/nhnacademy/exam/hotel/config/DataSourceConfig.java
+++ src/main/java/com/nhnacademy/exam/hotel/config/DataSourceConfig.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.exam.hotel.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,21 +10,32 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
 
+    @Value("${db.class.name}")
+    private String className;
+
+    @Value("${db.url}")
+    private String url;
+
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
     @Bean
     public DataSource dataSource() {
 
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://133.186.241.167:18080/nhn_exam_43");
-        dataSource.setUsername("nhn_exam_43");
-        dataSource.setPassword("sA4fxbhw!");
+        dataSource.setDriverClassName(className);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
 
-        dataSource.setInitialSize(100);     // 초기 커넥션 개수
-        dataSource.setMaxTotal(100);        // 최대 커넥션 개수
-        dataSource.setMinIdle(100);         // 최소 유휴 커넥션 개수
-        dataSource.setMaxIdle(100);         // 최대 유휴 커넥션 개수
+        dataSource.setInitialSize(10);
+        dataSource.setMaxTotal(50);
+        dataSource.setMinIdle(10);
+        dataSource.setMaxIdle(50);
 
-        // Connection 유효성 검사를 위한 설정
         dataSource.setTestOnBorrow(true);           // 커넥션 획득 전 테스트
         dataSource.setValidationQuery("SELECT 1");  // 커넥션 유효성 검사 쿼리
 

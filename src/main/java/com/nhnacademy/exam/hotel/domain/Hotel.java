8835bfa104190106caa549dca265258fccc52ae0
조승주 <tmdwn3318@gmail.com>
2025-01-06 09:22:12 +0900

    feat: hotel 엔티티 작성

--- src/main/java/com/nhnacademy/exam/hotel/domain/Hotel.java
+++ src/main/java/com/nhnacademy/exam/hotel/domain/Hotel.java
@@ -1,6 +1,27 @@
 package com.nhnacademy.exam.hotel.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
 public class Hotel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long hotelId;
 
+    @NotNull
+    @Length(max = 100)
+    private String name;
 }
